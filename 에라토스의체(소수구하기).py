2144e9892474a0ad9d19eaf5d8b0bc8034f6c886
Dochi <78398552+developer-Wonjun@users.noreply.github.com>
2021-02-06 15:42:20 +0900

    Eratosthenes

---
+++ 에라토스의체(소수구하기).py
@@ -0,0 +1,10 @@
+def eratosthenes(n):
+    multiples = set()
+    for i in range(2, n+1):
+        if i not in multiples:
+            yield i
+            multiples.update(range(i*i, n+1, i))
+
+
+num
+print(list(eratosthenes(100)))
